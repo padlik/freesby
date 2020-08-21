fb3b557ebbbba8a85b9627e856313041056f87fa
ppronko-sugarcrm <ppronko@sugarcrm.com>
2020-08-21 19:45:38 +0300

    Fixed errors in htpasswd generation

--- nginx/makehtpasswd.sh
+++ nginx/makehtpasswd.sh
@@ -3,8 +3,9 @@ set -e
 
 touch ${PASSWD_FILE}
 
-if [ -r ${USERS_FILE} ] then
-        for read username userpass do
+if [ -r ${USERS_FILE} ]; then
+        while read username userpass 
+	do
                 echo "Adding user ${username}"
                 htpasswd -b ${PASSWD_FILE} ${username} ${userpass}
                
