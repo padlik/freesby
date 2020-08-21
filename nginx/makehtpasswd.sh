029c335d8dd0c46577abdaff403655aa5434d217
ppronko-sugarcrm <ppronko@sugarcrm.com>
2020-08-21 19:32:24 +0300

    Bad env varilabe corrected

--- nginx/makehtpasswd.sh
+++ nginx/makehtpasswd.sh
@@ -3,10 +3,10 @@ set -e
 
 touch ${PASSWD_FILE}
 
-if [ -r ${USERS_LIST} ] then
+if [ -r ${USERS_FILE} ] then
         for read username userpass do
                 echo "Adding user ${username}"
                 htpasswd -b ${PASSWD_FILE} ${username} ${userpass}
                
-        done < ${USERS_LIST}
+        done < ${USERS_FILE}
 fi
