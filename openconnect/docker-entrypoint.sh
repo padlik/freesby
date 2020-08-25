01a2c14b5396ac8af80739bdc7b5bb84de91d9ea
padlik <propan@gmail.com>
2020-08-25 21:21:06 +0300

    ocserv from Debian with simplified configuration

--- openconnect/docker-entrypoint.sh
+++ openconnect/docker-entrypoint.sh
@@ -1,66 +1,26 @@
 #!/bin/bash
 
-if [ ! -f /etc/ocserv/certs/server-key.pem ] || [ ! -f /etc/ocserv/certs/server-cert.pem ]; then
-	# Check environment variables
-	if [ -z "$CA_CN" ]; then
-		CA_CN="VPN CA"
-	fi
 
-	if [ -z "$CA_ORG" ]; then
-		CA_ORG="Big Corp"
-	fi
+NGINX_HOST=${NGINX_HOST:-example.com} 
 
-	if [ -z "$CA_DAYS" ]; then
-		CA_DAYS=9999
-	fi
-
-	if [ -z "$SRV_CN" ]; then
-		SRV_CN="www.example.com"
-	fi
-
-	if [ -z "$SRV_ORG" ]; then
-		SRV_ORG="MyCompany"
-	fi
-
-	if [ -z "$SRV_DAYS" ]; then
-		SRV_DAYS=9999
-	fi
-
-	# No certification found, generate one
-	mkdir /etc/ocserv/certs
-	cd /etc/ocserv/certs
-	certtool --generate-privkey --outfile ca-key.pem
-	cat > ca.tmpl <<-EOCA
-	cn = "$CA_CN"
-	organization = "$CA_ORG"
-	serial = 1
-	expiration_days = $CA_DAYS
-	ca
-	signing_key
-	cert_signing_key
-	crl_signing_key
-	EOCA
-	certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca.pem
-	certtool --generate-privkey --outfile server-key.pem 
-	cat > server.tmpl <<-EOSRV
-	cn = "$SRV_CN"
-	organization = "$SRV_ORG"
-	expiration_days = $SRV_DAYS
-	signing_key
-	encryption_key
-	tls_www_server
-	EOSRV
-	certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
-
-	# Create users if required
-	if [ -r ${USERS_FILE} ] && [ ! -f /etc/ocserv/ocpasswd ]; then
-		while read username userpass
-		do
-			echo "Adding user ${username}"
-			echo "$userpass" | ocpasswd -c /etc/ocserv/ocpasswd -g All,Route ${username}
-		done < ${USERS_FILE}
-	fi
+if [ ! -d  /etc/letsencrypt/live/${NGINX_HOST} ]; then
+	mkdir -p /etc/letsencrypt/live/${NGINX_HOST}
+	cp /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/letsencrypt/live/${NGINX_HOST}
+	cp /etc/ssl/certs/ssl-cert-snakeoil.key /etc/letsencrypt/live/${NGINX_HOST}
 fi
+
+
+# Create users if required
+if [ -r ${USERS_FILE} ] && [ ! -f /etc/ocserv/ocpasswd ]; then
+	while read username userpass
+	do
+		echo "Adding user ${username}"
+		echo "$userpass" | ocpasswd -c /etc/ocserv/ocpasswd ${username}
+	done < ${USERS_FILE}
+fi
+
+echo "Creating configuration file" 
+envsubst < /etc/ocserv.conf.template > /etc/ocserv.conf
 
 # Open ipv4 ip forward
 sysctl -w net.ipv4.ip_forward=1
@@ -68,11 +28,6 @@ sysctl -w net.ipv4.ip_forward=1
 # Enable NAT forwarding
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-
-# Enable TUN device
-mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
-chmod 600 /dev/net/tun
 
 # Run OpennConnect Server
 exec "$@"
