aec69c90b7442366d9ca51d3115d0b6f71dcab47
padlik <propan@gmail.com>
2020-08-28 14:54:31 +0300

    Fine tuning on SSTP NAT

--- sstp/entrypoint.sh
+++ sstp/entrypoint.sh
@@ -7,12 +7,13 @@ stop_ok() {
 	exit 0
 }
 
-/usr/local/vpnserver/vpnserver start
-
-sleep 3s
-#disable DOS protection which is useless 
+/usr/local/vpnserver/vpnserver start && sleep 3s
+/usr/local/vpnserver/vpnserver stop && sleep 3s
+#disable DOS protection which is useless
 sed -i 's/\(\s*bool\s*DisableDos\s\).*$/\1true/' /usr/local/vpnserver/vpn_server.config
 sed -i 's/\(\s*bool\s*DisableDosProction\s\).*$/\1true/' /usr/local/vpnserver/vpn_server.config
+/usr/local/vpnserver/vpnserver start && sleep 3s
+
 
 /usr/local/vpnserver/vpncmd /SERVER localhost /CMD HubCreate $HUBNAME /PASSWORD:$HUBPASS
 
@@ -27,6 +28,8 @@ fi
 
 
 /usr/local/vpnserver/vpncmd /SERVER localhost /HUB:$HUBNAME /PASSWORD:$HUBPASS /CMD SecureNatEnable
+/usr/local/vpnserver/vpncmd /SERVER localhost /HUB:$HUBNAME /PASSWORD:$HUBPASS /CMD ExtOptionSet DisableIpRawModeSecureNAT /VALUE:true
+/usr/local/vpnserver/vpncmd /SERVER localhost /HUB:$HUBNAME /PASSWORD:$HUBPASS /CMD ExtOptionSet DisableKernelModeSecureNAT /VALUE:true
 /usr/local/vpnserver/vpncmd /SERVER localhost /CMD IPsecEnable /L2TP:no /L2TPRAW:no /ETHERIP:no /PSK:DUMMYPSK /DEFAULTHUB:$HUBNAME
 /usr/local/vpnserver/vpncmd /SERVER localhost /CMD OpenVpnEnable no /PORTS:1194
 /usr/local/vpnserver/vpncmd /SERVER localhost /CMD ServerPasswordSet $SERVERPASS
