2958f95595a6df0ef057f9d698eb1f8e6c08fa53
padlik <propan@gmail.com>
2020-09-02 12:59:57 +0300

    mend

--- certgen.sh
+++ certgen.sh
@@ -2,7 +2,7 @@
 echo "About to generate certificates"
 pushd ./certbot
 echo "Starting acme challenge server"
-docker-compose run acme -d
+docker-compose run -d acme
 sleep 3s
 echo "Genrating certificates"
 docker-compose run certbot
