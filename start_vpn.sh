c4e5652ead2c16d5d2bb1a473ab202cc847b9b4d
padlik <propan@gmail.com>
2020-09-02 15:25:37 +0300

    mend

--- start_vpn.sh
+++ start_vpn.sh
@@ -3,6 +3,7 @@
 echo "Starting vpn services"
 echo "Checking certificates..."
 ./certgen.sh && docker-compose up -d 
-
-
+echo "=============================="
+echo "Please visit https://${NGINX_HOST}/${FREESBY_PREFIX}/vpn for enabling OpenConnect VPN endpoint"
+echo "Please visit https://${NGINX_HOST}/${FREESBY_PREFIX}/sstp for enabling SSTP VPN endpoint"
 
